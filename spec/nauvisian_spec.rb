46c9f03c09b4b5f90e8444d857e5be9970fcd439
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-01-17 01:35:34 +0900

    Remove useless test

--- spec/nauvisian_spec.rb
+++ spec/nauvisian_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Nauvisian do
   it "has a version number" do
-    expect(Nauvisian::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Nauvisian::VERSION).not_to be_nil
   end
 end
