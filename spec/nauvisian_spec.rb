d09220eb41bed6dd81bf2bbfd7d6ac2010f9f7f9
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-01-24 19:49:52 +0900

    Add spec for inflector

--- spec/nauvisian_spec.rb
+++ spec/nauvisian_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Nauvisian do
+  it "has an inflector" do
+    expect(Nauvisian.inflector).to be_an_instance_of(Dry::Inflector)
+  end
+
   it "has a version number" do
     expect(Nauvisian::VERSION).not_to be_nil
   end
