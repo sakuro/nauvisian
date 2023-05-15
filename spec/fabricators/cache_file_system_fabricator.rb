21740f07abb6738da40b8ef1b43ec6da14780d39
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-05-15 14:02:59 +0900

    Fix random range

--- spec/fabricators/cache_file_system_fabricator.rb
+++ spec/fabricators/cache_file_system_fabricator.rb
@@ -3,7 +3,7 @@
 Fabricator(:cache_file_system, from: "Nauvisian::Cache::FileSystem") do
   transient :name, :ttl
   name { Faker::Lorem.word.downcase }
-  ttl { (rand(100 - 4) + 4) * 60 } # 4..100 minutes in seconds
+  ttl { rand(5..100) * 60 } # 5..100 minutes in seconds
   initialize_with do |transients|
     resolved_class.new(**transients)
   end
