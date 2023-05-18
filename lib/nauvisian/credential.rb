9c68d306571ff65a1dc9ea17377ca61244c72f21
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-05-18 14:34:22 +0900

    Freeze string values

--- lib/nauvisian/credential.rb
+++ lib/nauvisian/credential.rb
@@ -11,12 +11,13 @@ module Nauvisian
     end
 
     def self.from_env
+      # NOTE: values of ENV are already frozen
       self[username: ENV.fetch("FACTORIO_SERVICE_USERNAME"), token: ENV.fetch("FACTORIO_SERVICE_TOKEN")]
     end
 
     def self.from_player_data_file(player_data_file_path: Nauvisian.platform.user_data_directory / "player-data.json")
       data = JSON.load_file(player_data_file_path)
-      self[username: data["service-username"], token: data["service-token"]]
+      self[username: data["service-username"].freeze, token: data["service-token"].freeze]
     end
   end
 end
