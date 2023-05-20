6523b3db36de4a609666efcc5263c98816dedffd
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2023-05-20 17:51:30 +0900

    Support --mod-directory

--- lib/nauvisian/cli/commands/mod/installed.rb
+++ lib/nauvisian/cli/commands/mod/installed.rb
@@ -12,9 +12,12 @@ module Nauvisian
 
           desc "List installed MODs"
           option :format, default: "plain", values: Nauvisian::CLI::Lister.all, desc: "Output format"
+          option :mod_directory, desc: "Directory where MODs are installed", required: false, default: Nauvisian.platform.mod_directory.to_s
 
           def call(*, **options)
-            mods = Nauvisian::ModList.load.sort
+            mod_directory = Pathname(options[:mod_directory])
+            mod_list_path = mod_directory / "mod-list.json"
+            mods = Nauvisian::ModList.load(mod_list_path).sort
             rows = mods.map {|mod, enabled| {"Name" => mod.name, "Enabled" => enabled} }
 
             lister = Nauvisian::CLI::Lister.for(options[:format].to_sym).new(%w(Name Enabled))
--- lib/nauvisian/cli/commands/mod/settings/dump.rb
+++ lib/nauvisian/cli/commands/mod/settings/dump.rb
@@ -11,9 +11,13 @@ module Nauvisian
             include MessageHelper
 
             desc "Dump MOD settings"
+            option :mod_directory, desc: "Directory where MODs are installed", required: false, default: Nauvisian.platform.mod_directory.to_s
 
-            def call(**)
-              puts Nauvisian::ModSettings.load.to_json
+            def call(**options)
+              mod_directory = Pathname(options[:mod_directory])
+              mod_settings_path = mod_directory / "mod-settings.dat"
+              settings = Nauvisian::ModSettings.load(mod_settings_path)
+              puts settings.to_json
             rescue => e
               message(e)
               exit 1
